dc1529a357315a2e8c1a04e49e352495eda7b00f
Navnit Tiwari <42030960+navnittiwari@users.noreply.github.com>
2020-09-19 20:37:06 +0530

    in 366

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -12,14 +11,42 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser : true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser : true, useUnifiedTopology : true});
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "welcome to your todo list"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add new items"
+});
+
+const item3 = new Item({
+  name: "hit <-- to delete an option"
+});
+
+const defaultItems = [item1, item2, item3];
+
+//Item.insertMany(defaultItems, function(err){
+//  if(err){
+//    console.log(err);
+//  }else{
+//    console.log("items saved to DB successfully");
+//  }
+//});
 
 app.get("/", function(req, res) {
-
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+  
+  Item.find({}, function(err, foundItems){
+    res.render("list", {listTitle: "Today", newListItems: foundItems});
+  });
+      
 });
 
 app.post("/", function(req, res){
