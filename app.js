3ce82df0860e2a4f18804e79979af5272ed63e93
Navnit Tiwari <42030960+navnittiwari@users.noreply.github.com>
2020-09-27 21:47:32 +0530

    368 completed

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ const defaultItems = [item1, item2, item3];
 
 
 app.get("/", function (req, res) {
+  
   Item.find({}, function (err, foundItems) {
     if (foundItems.length === 0) {
       Item.insertMany(defaultItems, function (err) {
@@ -56,15 +57,27 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name: itemName
+  });
+
+  item.save();
+
+  res.redirect("/");
+});
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if(!err){
+      console.log("item deleted successfully.");
+      res.redirect("/");
+    }
+  });
+
 });
 
 app.get("/work", function (req, res) {
